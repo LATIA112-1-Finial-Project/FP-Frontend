7170af6a4c96c53139a2921d004cbb7eb9a64b7e
KutsunaSubaRya <index20010928@gmail.com>
2023-12-11 02:33:42 +0800

    feat: add dashboard, arxiv router, menubar, sidebar, arxiv api
    - dashboard welcome page
    - dashboard
        - menu Arxiv router link
        - sidebar component
    - Arxiv data fetch api
    - isFetching loading page

--- src/views/user/dashboard.vue
+++ src/views/user/dashboard.vue
@@ -1,42 +1,62 @@
 <template>
-  <div class="box mt-4">
-    <t-head-menu default-value="2-1" expand-type="popup">
-      <template #logo>
-        <div class="font-bold text-xl ml-8"> 大學排名與熱門領域討論站 </div>
-      </template>
-      <t-menu-item value="1">
-        綜合討論區
-      </t-menu-item>
-      <t-submenu value="2" title="大學排名分析">
-        <t-menu-item value="2-1"> 綜合分數 </t-menu-item>
-        <t-menu-item value="2-2"> 學校聲望 </t-menu-item>
-        <t-menu-item value="2-3"> 師資聲望 </t-menu-item>
-      </t-submenu>
-      <t-menu-item value="3">
-        論文領域熱門分析
-      </t-menu-item>
-      <template #operations>
-        <t-button variant="text" shape="square" class="mr-4">
-          <template #icon><User1Icon /></template>
-        </t-button>
-        <t-button variant="text" shape="square" class="mr-8" @click="handleLogout">
-          <template #icon><LogoutIcon /></template>
-        </t-button>
-      </template>
-    </t-head-menu>
-  </div>
-  <div class="ml-12">
-    hi {{userInfo.username}}
-  </div>
+  <t-layout>
+    <t-header class="flex items-center h-[calc(72px)]">
+      <t-head-menu default-value="2-1" expand-type="popup">
+        <template #logo>
+          <router-link :to="{name: 'showDashboard'}">
+            <div class="font-bold text-xl ml-8" @click="pageVal=false"> 大學排名與熱門領域討論站</div>
+          </router-link>
+        </template>
+        <t-menu-item value="1" class="text-md font-semibold">
+          綜合討論區
+        </t-menu-item>
+        <t-submenu value="2" title="大學排名分析" class="text-md font-semibold">
+          <t-menu-item value="2-1"> 綜合分數</t-menu-item>
+          <t-menu-item value="2-2"> 學校聲望</t-menu-item>
+          <t-menu-item value="2-3"> 師資聲望</t-menu-item>
+        </t-submenu>
+        <t-menu-item value="3" @click="handleMenuClick" class="text-md font-semibold">
+            論文領域熱門分析
+        </t-menu-item>
+        <template #operations>
+          <t-button variant="text" shape="square" class="mr-4">
+            <template #icon>
+              <User1Icon/>
+            </template>
+          </t-button>
+          <t-button variant="text" shape="square" class="mr-8" @click="handleLogout">
+            <template #icon>
+              <LogoutIcon/>
+            </template>
+          </t-button>
+        </template>
+      </t-head-menu>
+    </t-header>
+    <t-layout class="flex">
+      <router-view v-if="pageVal">
+      </router-view>
+      <Content v-if="!pageVal" class="h-[calc(100vh-72px)] overflow-y-auto py-8 px-12 flex items-center justify-center flex-col">
+        <t-card hover-shadow header-bordered class="flex items-center justify-center font-bold text-3xl w-2/3 h-2/3 rounded-3xl">
+          <div>
+            Hi, {{ authStore.userInfo.username }}
+          </div>
+          <div>
+            歡迎來到大學排名與熱門領域討論站
+          </div>
+        </t-card>
+      </Content>
+    </t-layout>
+  </t-layout>
 </template>
 
-<script setup lang="ts" >
+<script setup lang="ts">
 import {useAuthStore} from "@/stores/auth.ts";
-import {onMounted} from "vue";
-import { useFetch } from "@vueuse/core";
+import {onMounted, ref} from "vue";
+import {useFetch} from "@vueuse/core";
 import {User1Icon, LogoutIcon} from "tdesign-icons-vue-next";
 import {MessagePlugin} from "tdesign-vue-next";
 import {useRouter} from "vue-router";
+import {Content} from "tdesign-vue-next";
 
 const authStore = useAuthStore();
 
@@ -64,14 +84,14 @@ interface UserInfo {
 // get user info from GET import.meta.env.VITE_API_ENDPOINT + /api/v1/user_info
 
 const handleUserInfo = async () => {
-  const { data } = await useFetch(`${import.meta.env.VITE_API_ENDPOINT}` + '/auth/user_info', {
+  const {data} = await useFetch(`${import.meta.env.VITE_API_ENDPOINT}` + '/auth/user_info', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${userInfo.token}`
     }
   }).get().json<UserInfoResData>()
-  if(data.value) {
+  if (data.value) {
     if (data.value.msg !== 'success') {
       return
     }
@@ -86,13 +106,13 @@ const handleUserInfo = async () => {
 
 
 const handleLogout = async () => {
-  const { data } = await useFetch(`${import.meta.env.VITE_API_ENDPOINT}` + '/logout', {
+  const {data} = await useFetch(`${import.meta.env.VITE_API_ENDPOINT}` + '/logout', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     }
   }).get().json<generalResData>()
-  if(data.value) {
+  if (data.value) {
     if (data.value.msg !== 'success') {
       return
     }
@@ -102,13 +122,29 @@ const handleLogout = async () => {
   }
 }
 
+// click 綜合討論區 menu-item value = 1, router-view to /user/discussion
+// click 大學排名分析 menu-item value = 2, router-view to /user/ranking
+// click 論文領域熱門分析 menu-item value = 3, router-view to /user/arxiv
+
+
+// use v-if to show router-view or default page (Content)
+// pageVal = 1, router-view to /user/discussion
+// pageVal = 2, router-view to /user/ranking
+// pageVal = 3, router-view to /user/arxiv
+const pageVal = ref(false)
+const handleMenuClick = () => {
+  pageVal.value = true
+  router.push({name: 'showArxiv'})
+}
+
+
 // if no token, redirect to login page
 if (!authStore.isLoggedIn) {
   router.replace({name: 'userLogin'})
 }
 
 onMounted(() => {
-  if(!authStore.isLoggedIn){
+  if (!authStore.isLoggedIn) {
     router.replace({name: 'userLogin'})
   }
   handleUserInfo()
