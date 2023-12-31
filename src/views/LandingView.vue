c63d44819148528399929a988d70511412735944
KutsunaSubaRya <47558875+KutsunaSubaRya@users.noreply.github.com>
2023-12-31 18:39:37 +0800

    fix: fix web title (#12)

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -5,7 +5,7 @@
           class="absolute inset-0 bg-opacity-30 font-electrolyte bg-gradient-to-bl from-black to-transparent bg-gradient-to-tl from-black to-transparent opacity-75"></div>
       <div
           class="flex flex-col items-center justify-center w-1/2 h-1/2 p-8 bg-opacity-5 rounded-3xl relative z-10 bg-black">
-        <div class="text-6xl text-white">LATIA 112-1 期末專題</div>
+        <div class="text-6xl text-white">歡迎來到</div>
         <div class="text-6xl text-white my-8 mb-20">大學排名與熱門領域討論站</div>
         <router-link :to="{ name: 'userLogin' }">
           <Button theme="success" shape="round" class="flex items-center">
